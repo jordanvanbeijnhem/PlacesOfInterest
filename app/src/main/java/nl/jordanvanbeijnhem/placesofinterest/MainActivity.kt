bc93da7a9a321d4a90db83f724b0d271ac01d4e1
jordan van beijnhem <jordan.van.beijnhem@hva.nl>
2019-08-25 15:38:02 +0200

    Initial commit

---
+++ app/src/main/java/nl/jordanvanbeijnhem/placesofinterest/MainActivity.kt
@@ -0,0 +1,30 @@
+package nl.jordanvanbeijnhem.placesofinterest
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+import nl.jordanvanbeijnhem.placesofinterest.adapter.PlaceAdapter
+import nl.jordanvanbeijnhem.placesofinterest.model.Place
+
+class MainActivity : AppCompatActivity() {
+
+    private val places = arrayListOf<Place>()
+    private val placeAdapter = PlaceAdapter(places)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        initViews()
+    }
+
+    private fun initViews() {
+        rvPlaces.layoutManager = StaggeredGridLayoutManager(2, StaggeredGridLayoutManager.VERTICAL)
+        rvPlaces.adapter = placeAdapter
+
+        for (i in Place.PLACE_NAMES.indices) {
+            places.add(Place(Place.PLACE_NAMES[i], Place.PLACE_RES_DRAWABLE_IDS[i]))
+        }
+        placeAdapter.notifyDataSetChanged()
+    }
+}
